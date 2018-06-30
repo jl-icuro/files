f6514550d9f8e1db595744c00e11d80efe108d0e
jl-icuro <40698758+jl-icuro@users.noreply.github.com>
2018-06-29 17:48:55 -0700

    Update connectos1.sh

--- packages/connectos1.sh
+++ packages/connectos1.sh
@@ -3,10 +3,14 @@ sudo sed -i 's/#interface=/interface=eth0/g' /etc/dnsmasq.conf
 
 sudo sed -i 's/#dhcp-range=192.168.0.50,192.168.0.150,12h/dhcp-range=192.168.1.50,192.168.1.150,12h/g' /etc/dnsmasq.conf
 
+
+echo "Stopping dnsmasp..."
 sudo systemctl stop dnsmasq
 
 sleep 5
 
+echo "Starting dnsmasq..."
 sudo systemctl start dnsmasq
 
+echo "Starting journalctl..."
 journalctl -fu dnsmasq
