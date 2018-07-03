a26e215b0d74b4173a835127a0f191dfecd9b814
jl-icuro <40698758+jl-icuro@users.noreply.github.com>
2018-07-02 18:39:49 -0700

    Update runbosch.sh

--- packages/runbosch.sh
+++ packages/runbosch.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-xterm -e bash -c '
-
-source /home/autoware/ros_ws/imu_ws/devel/setup.bash;
+source /home/autoware/ros_ws/imu_ws/devel/setup.bash
 
 roslaunch rtimulib_ros rtimulib_ros.launch
-
-' &
