da390214204ea1371015232cca148875241d365d
jl-icuro <40698758+jl-icuro@users.noreply.github.com>
2018-08-02 17:10:58 -0700

    Update ROSMulti.sh

--- packages/ROSMulti.sh
+++ packages/ROSMulti.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
 
-read -p "Enter Computer IP Address: " SELF_IP
+MULTI_SET=0
 
-read -p "Enter Host Computer: " HOST_IP
+if [ $ROS_IP ]
+then
+   MULTI_SET=1
+fi
 
-echo "export ROS_IP=$SELF_IP" >> ~/.bashrc
-echo "export ROS_MASTER_URI=http://$HOST_IP:11311" >> ~/.bashrc
-
-echo "Don't forget to source .bashrc!"
+if [ $MULTI_SET -eq 1 ]
+then
+   read -p "Single 0 Multiple 1: " MULTI_OPT
+   if [ $MULTI_OPT -eq 1 ]
+   then
+      "Added ROS_IP and ROS_MASTER_URI"
+      sed -i '/ROS_IP=/d' /home/$USER/.bashrc
+      sed -i '/ROS_MASTER_URI=/d' /home/$USER/.bashrc
+      read -p "Enter Computer IP Address: " SELF_IP
+      read -p "Enter Host Computer: " HOST_IP
+      echo "export ROS_IP=$SELF_IP" >> /home/$USER/.bashrc
+      echo "export ROS_MASTER_URI=http://$HOST_IP:11311" >> /home/$USER/.bashrc
+   else
+      echo "Deleted ROS_IP and ROS_MASTER_URI"
+      sed -i '/ROS_IP=/d' /home/$USER/.bashrc
+      sed -i '/ROS_MASTER_URI=/d' /home/$USER/.bashrc
+   fi
+else
+   read -p "Single 0 Multiple 1: " MULTI_OPT
+   if [ $MULTI_OPT -eq 1 ]
+   then
+      echo "Added ROS_IP and ROS_MASTER_URI"
+      read -p "Enter Computer IP Address: " SELF_IP
+      read -p "Enter Host Computer: " HOST_IP
+      echo "export ROS_IP=$SELF_IP" >> /home/$USER/.bashrc
+      echo "export ROS_MASTER_URI=http://$HOST_IP:11311" >> /home/$USER/.bashrc
+   else
+      echo "Deleted ROS_IP and ROS_MASTER_URI"
+   fi
+fi
