0c169dc0509a99887defc4a63cf9c94c597c3d1d
jl-icuro <40698758+jl-icuro@users.noreply.github.com>
2018-08-13 09:36:47 -0700

    Update installTensorFlow.sh

--- packages/installTensorFlow.sh
+++ packages/installTensorFlow.sh
@@ -35,6 +35,9 @@ sudo apt-get update
 #sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
 #sudo chmod a+r /usr/local/cuda/include/cudnn.h
 
+# Export USER
+export USER=autoware
+
 # Install Dependencies
 sudo apt-get install -y python-pil python-lxml python-tk python-pip python-dev
 pip install --user Cython
@@ -43,7 +46,6 @@ pip install --user jupyter
 pip install --user matplotlib
 
 # Clone tensorflow models
-sudo apt-get install -y git
 cd ~
 mkdir tensorflow
 cd tensorflow
@@ -60,20 +62,38 @@ wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-li
 unzip protoc-3.5.1-linux-x86_64.zip
 
 # Link protobuf and tensorflow
+export PATH=$PATH:/usr/local/cuda-9.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/home/$USER/protobuf/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-9.0/lib64:
+export PYTHONPATH=$PYTHONPATH:/home/$USER/tensorflow/models/research:/home/$USER/tensorflow/models/research/slim
+
+# Add to bashrc
+cd ~
+echo "export PATH=$PATH:/usr/local/cuda-9.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/home/$USER/protobuf/bin" >> .bashrc
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-9.0/lib64:" >> .bashrc
+echo "export PYTHONPATH=$PYTHONPATH:/home/$USER/tensorflow/models/research:/home/$USER/tensorflow/models/research/slim" >> .bashrc
 
 # Compile protobuf libraries
 cd /home/$USER/tensorflow/models/research
-export PATH=$PATH:/usr/local/cuda-9.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/home/$USER/protobuf/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-9.0/lib64:
-export PYTHONPATH=/home/$USER/caffe/python:$PYTHONPATH:/home/$USER/tensorflow/models/research:/home/$USER/tensorflow/models/research/slim
 protoc object_detection/protos/*.proto --python_out=.
 
 # Install tensorflow
-sudo pip install tensorflow-gpu
+cd ~
+wget http://download1133.mediafire.com/643xro8rtctg/j2c9o5yif9dn3cu/tensorflow-1.7.0-cp27-cp27mu-linux_aarch64.whl
+sudo -H pip install tensorflow-1.7.0-cp27-cp27mu-linux_aarch64.whl
+rm tensorflow-1.7.0-cp27-cp27mu-linux_aarch64.whl
 
 # Install COCO API
 cd ~
 git clone https://github.com/cocodataset/cocoapi.git
 cd cocoapi/PythonAPI
 make
-cp -r pycocotools ~/tensorflow/models/research/
+cp -r pycocotools ~/tensorflow/models/research
+
+# Install tf_trt_models
+cd ~
+mkdir tensorrt
+cd tensorrt
+git clone https://github.com/NVIDIA-Jetson/tf_trt_models.git
+cd tf_trt_models
+git checkout 3cffe63fa607a0811f90aedad3a6eb6ea3ad5223
+./install.sh
